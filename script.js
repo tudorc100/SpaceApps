e61837b52d3e20112bf22436d9b0c9c42f1c01d7
tudorc100 <tudorcampan@gmail.com>
2023-10-08 10:29:57 +0300

    Final version

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let scene, camera, renderer, moon, controls;
 const intersectedObjects = [];  // Ensure it's initialized here
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();  // Ensure it's initialized here
+let moonTexture, heatmapTexture;
 function init() {
     // Scene
     scene = new THREE.Scene();
@@ -19,46 +20,31 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const starSphereRadius = 50; // Adjust this value as needed. Make sure it's larger than other objects in your scene.
+    // Stars
+    const starSphereRadius = 50;
     const starSphereGeometry = new THREE.SphereGeometry(starSphereRadius, 64, 64);
-
-    const starsMaterial = new THREE.PointsMaterial({ color: 0xFFFFFF, size: 0.22 }); // Adjust the size as needed
-
+    const starsMaterial = new THREE.PointsMaterial({ color: 0xFFFFFF, size: 0.22 });
     const starsVertices = [];
 
     for (let i = 0; i < 1000; i++) {
         const theta = 2 * Math.PI * Math.random();
         const phi = Math.acos(2 * Math.random() - 1);
-
         const x = starSphereRadius * Math.sin(phi) * Math.cos(theta);
         const y = starSphereRadius * Math.sin(phi) * Math.sin(theta);
         const z = starSphereRadius * Math.cos(phi);
-
         starsVertices.push(x, y, z);
     }
 
     const starsGeometry = new THREE.BufferGeometry().setAttribute('position', new THREE.Float32BufferAttribute(starsVertices, 3));
     const starSphere = new THREE.Points(starsGeometry, starsMaterial);
-
     scene.add(starSphere);
 
-    // // Add stars
-    // for (let i = 0; i < 1000; i++) {
-    //     const starGeometry = new THREE.SphereGeometry(0.005, 24, 24);
-    //     const starMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
-    //     const star = new THREE.Mesh(starGeometry, starMaterial);
-    //
-    //     star.position.x = (Math.random() - 0.5) * 10;
-    //     star.position.y = (Math.random() - 0.5) * 10;
-    //     star.position.z = (Math.random() - 0.5) * 10;
-    //
-    //     scene.add(star);
-    // }
-
-    // Moon
-    const geometry = new THREE.SphereGeometry(1, 32, 32);
+    // Moon Textures
     const loader = new THREE.TextureLoader();
-    const moonTexture = loader.load('moon_texture.jpg');
+    moonTexture = loader.load('moonSmall.jpg');
+    heatmapTexture = loader.load('download.png');
+
+    const geometry = new THREE.SphereGeometry(1, 32, 32);
     const material = new THREE.MeshStandardMaterial({ map: moonTexture });
     moon = new THREE.Mesh(geometry, material);
     scene.add(moon);
@@ -66,7 +52,6 @@ function init() {
     // Light
     const ambientLight = new THREE.AmbientLight(0x555555);
     scene.add(ambientLight);
-
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.position.set(5, 3, 5);
     scene.add(directionalLight);
@@ -87,10 +72,29 @@ function init() {
         }
     });
 
-    // Add mouse move listener for the hover effect
-    //document.addEventListener('mousemove', onMouseMove, false);
+    // Mouse event listeners
     window.addEventListener('click', onMouseClick);
+
+    // Button to switch texture
+    const switchButton = document.createElement('button');
+    switchButton.innerText = 'Switch Texture';
+    switchButton.style.position = 'fixed';
+    switchButton.style.top = '10px';
+    switchButton.style.left = '10px';
+    switchButton.style.zIndex = '1000';
+    switchButton.addEventListener('click', switchTexture);
+    document.body.appendChild(switchButton);
 }
+
+function switchTexture() {
+    if (moon.material.map === moonTexture) {
+        moon.material.map = heatmapTexture;
+    } else {
+        moon.material.map = moonTexture;
+    }
+    moon.material.needsUpdate = true;
+}
+
 function onMouseClick(event) {
     event.preventDefault();
 
@@ -102,13 +106,24 @@ function onMouseClick(event) {
 
     const infoBox = document.getElementById('infoBox');
 
-    if (intersects.length > 0) {
-        // Update details in the pop-up
-        const detailsDiv = document.getElementById('moonquakeDetails');
-        detailsDiv.style.display = 'block';
+    // Flag to check if details box is currently visible
+    var isDetailsVisible = false;
 
-        const detailsText = document.getElementById('quakeDetails');
-        detailsText.innerHTML = `
+    if (intersects.length > 0) {
+        // If details box is visible, animate it out
+        if (isDetailsVisible) {
+            detailsDiv.style.right = '-200px';
+            isDetailsVisible = false;
+        }
+
+        // Use a timeout to delay the in animation
+        setTimeout(function() {
+            // Update details in the pop-up
+            const detailsDiv = document.getElementById('moonquakeDetails');
+            detailsDiv.style.display = 'block';
+
+            const detailsText = document.getElementById('quakeDetails');
+            detailsText.innerHTML = `
             Date: ${intersects[0].object.userData.Date}<br>
             Latitude: ${intersects[0].object.userData.Lat}<br>
             Longitude: ${intersects[0].object.userData.Long}<br>
@@ -116,29 +131,44 @@ function onMouseClick(event) {
             Source: ${intersects[0].object.userData.Source}
         `;
 
-        // Show and animate the corresponding ring sprite
-        if (!intersects[0].object.userData.isRing) {
-            const ringSprite = intersectedObjects.find(obj => obj.userData.isRing && obj.userData.parent === intersects[0].object);
-            console.log("Detected Ring Sprite:", ringSprite); // Add this line
+            // Show and animate the corresponding ring sprite
+            if (!intersects[0].object.userData.isRing) {
+                const ringSprite = intersectedObjects.find(obj => obj.userData.isRing && obj.userData.parent === intersects[0].object);
 
-            if (ringSprite) {
-                ringSprite.material.opacity = 0.8;  // Set to visible
-                ringSprite.scale.set(0.05, 0.05, 0.05);  // Reset scale
+                if (ringSprite) {
+                    ringSprite.material.opacity = 0.8;  // Set to visible
+                    ringSprite.scale.set(0.05, 0.05, 0.05);  // Reset scale
 
-                // Start the animation
-                gsap.to(ringSprite.scale, {
-                    x: 0.1,  // Adjust as needed
-                    y: 0.1,
-                    duration: 1,
-                    ease: 'power1.out',
-                    onComplete: () => {
-                        ringSprite.material.opacity = 0;  // Hide after animation
-                    }
-                });
+                    // Start the animation
+                    gsap.to(ringSprite.scale, {
+                        x: 0.1,  // Adjust as needed
+                        y: 0.1,
+                        duration: 1,
+                        ease: 'power1.out',
+                        onComplete: () => {
+                            ringSprite.material.opacity = 0;  // Hide after animation
+                        }
+                    });
+                }
             }
-        }
+
+            // Show detailsDiv by sliding it in from the right
+            detailsDiv.style.right = '10px';
+            isDetailsVisible = true;
+        }, 300);  // Adjust delay as needed
     }
+
 }
+
+// Get the close button
+var closeButton = document.getElementById('closeButton');
+
+// Add click event listener to close button
+closeButton.addEventListener('click', function() {
+    // Hide detailsDiv by sliding it out to the right
+    detailsDiv.style.right = '-200px';
+});
+
 
 
 function processData(data) {
